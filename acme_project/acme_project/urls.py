b82ddf4b4aa7caf7e66686e3f3dbdce07f1a62ff
Andrey_Petrov <PetrovKRS@yandex.ru>
2023-09-05 12:41:18 +0300

    add pic to birthday list

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,12 +1,15 @@
+# Импортируем настройки проекта.
+from django.conf import settings
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
+
 # Импортируем класс модели Birthday.
 from .models import Birthday
+from .validators import real_age
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 # Для использования формы с моделями меняем класс на forms.ModelForm.
@@ -15,5 +22,25 @@ class BirthdayForm(forms.ModelForm):
         # Указываем, что надо отобразить все поля.
         fields = '__all__'
         widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
+            'birthday': forms.DateInput(attrs={'type': 'date'}),
+            'validators': (real_age,),
         }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
